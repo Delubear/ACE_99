592e0c14eeed5512cf672a0d9a7823e113479b33
aqualung99 <joelhunter@hotmail.com>
2021-10-08 20:54:24 -0700

    Pyreals have weight and start amount now 200p
    Characters with 10 strength can't hold much more than 200p, so I'm going with that for now.

---
+++ PyrealsHaveWeight/PyrealsHaveWeight.sql
@@ -0,0 +1,9 @@
+/* Per-Item in stack encumberance */
+UPDATE `weenie_properties_int`
+SET `value` = 1
+WHERE `object_Id` = 273 AND `type` = 13;
+
+/* Absolute encumberance(?) */
+UPDATE `weenie_properties_int`
+SET `value` = 1
+WHERE `object_Id` = 273 AND `type` = 5;
